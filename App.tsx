446f28ce7bf47ee0bda1b9de7942dc25b128bf7a
Fabio Reis <fj.reis@gmail.com>
2023-07-14 19:57:22 -0400

    feat: added redux and some others adjustments

--- App.tsx
+++ App.tsx
@@ -3,28 +3,32 @@ import {NavigationContainer} from "@react-navigation/native";
 import {createStackNavigator} from "@react-navigation/stack";
 
 import Home from "./src/Home/Home"
-import Details from "./src/Details";
+import Details from "./src/Details/Details";
+
+import { Provider } from "react-redux";
+import store from "./src/storage/redux";
 
 const App = () => {
   
   const Stack = createStackNavigator();
-
   
   return(
-    <NavigationContainer>
-      <Stack.Navigator
-         screenOptions={{
-          cardStyle: {
-            backgroundColor: '#FFF',
-          },
-          animationEnabled: false,
-          //headerShown: false,
-        }}       
-      >
-        <Stack.Screen name="Repositório" component={Home} />
-        <Stack.Screen name="Details" component={Details}/>        
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{
+            cardStyle: {
+              backgroundColor: '#FFF',
+            },
+            animationEnabled: false,
+            //headerShown: false,
+          }}       
+        >
+          <Stack.Screen name="Repositório" component={Home} />
+          <Stack.Screen name="Details" component={Details}/>        
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   )
 }
 
