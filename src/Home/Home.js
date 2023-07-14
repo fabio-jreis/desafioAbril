87f01f798a6d8ef9d8cf7ec7b2fc80203dc8d96f
Fabio Reis <fj.reis@gmail.com>
2023-07-14 17:34:55 -0400

    feat: added ready repository and show in home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,19 +1,98 @@
-import React from "react";
-import { Container, Title, Search, Icon } from "./styles";
+import React, { useEffect, useState } from "react";
+import api from "../services/api";
+import { Container, Title, Search, Icon, Repos, Divider,
+    ContainerItem, ContainerAvatarTitle, Avatar, TitleContainer, TitleItem, Subtitle, NumOfStar 
 
-import { View, Text, StyleSheet, Image } from 'react-native'
+} from "./styles";
+
+import { View, Text, StyleSheet, Image, FlatList } from 'react-native'
 import { TextInput } from "react-native-gesture-handler";
 
 import findIcon from "../../assets/images/magnifyingglass.png"
 
 export default function Home() {
+
+    const [input, setInput] = useState("Fabio")
+    const [data, setData] = useState(null)
+    const [loading, setLoading] = useState(false)
+    const [finalList, setFinalList] = useState(false)
+    const [page, setPage] = useState(1);
+
+    const readRepo = () => {
+        if(input !== "") {
+            setData([])
+            setLoading(true)
+            setFinalList(false)
+            api.get(`search/repositories?q=${input}&per_page=20`).then(response => {
+               var items = response.data.items;
+               setData(items)
+               
+               if(response.data.items.length < 20) {
+                setFinalList(true)
+               } else {
+                setPage(page + 1)
+               }
+             }).catch(e => {
+               console.log('erro 01 ' + e)
+             }).finally(() => {
+               setLoading(false)
+             })        
+        }
+    }
+
+    const readMoreRepo = () => {
+    }
+
+    const RepoItem = ({data}) => {
+
+        return (
+            <ContainerItem
+            onPress={{}}
+            >
+            <ContainerAvatarTitle>
+                <Avatar
+                testID='avatar'
+                source={{
+                    uri: data.owner.avatar_url,
+                }} />
+                <TitleContainer>
+                <TitleItem testID='name'>{data.name}</TitleItem>
+                <Subtitle testID='owner'>{data.owner.login}</Subtitle>
+                </TitleContainer>
+            </ContainerAvatarTitle>
+            <NumOfStar testID='stars'>{data.stargazers_count}</NumOfStar>
+            </ContainerItem>
+        )
+    }
+
     return(
         <Container>
             <Title></Title>
             <Search>
                 <Icon source={findIcon} />
-                <TextInput placeholder="Busca por Repositórios"/>                
+                <TextInput 
+                    placeholder="Busca por Repositórios"
+                    value={input}
+                    onChangeText={input => {
+                        setInput(input);
+                    }} 
+                    onBlur={ () => {readRepo()} }   
+                />                
             </Search>
+
+            <FlatList
+                data={data}
+                keyExtractor={data => data.id}
+                renderItem={({ item }) => (
+                    <RepoItem data={item} />
+                )}
+                ItemSeparatorComponent={<Divider />}
+                refreshing={loading}
+                onRefresh={() => readRepo()}
+                onEndReachedThreshold={0.25}
+                onEndReached={() => readMoreRepo()}
+                showsVerticalScrollIndicator={false}                
+            />
         </Container>
     )
 }
--- src/Home/styles.js
+++ src/Home/styles.js
@@ -21,3 +21,76 @@ export const Search = styled.View`
     background: rgba(118, 118, 128, 0.12);
     border-radius: 10px;
 `;
+
+export const Divider = styled.View`
+width: 100%;
+height: 1px;
+background: #EBEBEB;
+border-radius: 2px;
+margin-top: 12px;
+margin-bottom: 10px;
+`;
+
+export const ContainerItem =  styled.TouchableOpacity`
+    width: 100%;
+    flex-direction: row;
+    justify-content: space-between;
+    margin-top: 15px;
+  `;
+
+export const ContainerAvatarTitle = styled.View`
+    flex-direction: row;
+    flex: 8;
+  `;
+
+export const Avatar =  styled.Image`
+    width: 52px;
+    height: 52px;
+    background: #D9D9D9;
+    border-radius: 50px;
+    margin-right: 12px;
+  `;
+
+export const TitleContainer = styled.View`
+    flex: 1;
+    margin-right: 5px;
+  `;
+
+export const TitleItem = styled.Text.attrs({
+    numberOfLines: 1
+  })`
+    font-family: 'SF Pro Text';
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 24px;
+    letter-spacing: -0.38px;
+    color: '#000';
+    margin-bottom: 1px;
+  `;
+
+export const Subtitle = styled.Text.attrs({
+    numberOfLines: 1
+  })`
+    font-family: 'SF Pro Text';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 24px;
+    color: '#48484A';
+  `;
+
+export const NumOfStar = styled.Text.attrs({
+    numberOfLines: 1
+  })`
+    font-family: 'SF Pro Text';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+    letter-spacing: -0.078px;
+    color: '#48484A';
+    flex: 2;
+    text-align: right;
+    margin-right: 4px;
+  `;
